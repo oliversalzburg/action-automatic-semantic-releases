aef5ef8bc1dd0aae5f67d5e6fdd7b2ad8955d80e
Oliver Salzburg <oliver.salzburg@gmail.com>
2025-02-22 17:08:39 +0100

    feat: Support version suffixes

--- source/version.ts
+++ source/version.ts
@@ -10,6 +10,10 @@ const getRootVersion = (currentVersion: string) => {
   return currentVersion.replace(/^v?(\d+\.\d+\.\d+)-?.*$/, "$1");
 };
 
+const getVersionExtension = (currentVersion: string) => {
+  return currentVersion.replace(/^v?\d+\.\d+\.\d+(-?.*)$/, "$1");
+};
+
 const bumpVersion = (rootVersion: string, level: "major" | "minor" | "patch") => {
   const parts = rootVersion.split(".");
 
@@ -40,16 +44,31 @@ const bumpVersion = (rootVersion: string, level: "major" | "minor" | "patch") =>
  */
 export const suggestVersions = (currentVersion: string) => {
   const rootVersion = getRootVersion(currentVersion);
+  const versionExtension = getVersionExtension(currentVersion);
+
   return {
     current: currentVersion,
     root: rootVersion,
+    extension: versionExtension,
     dev: [
       rootVersion,
       "-dev",
       process.env.GITHUB_SHA ? `-${String(process.env.GITHUB_SHA).substring(0, 7)}` : "",
     ].join(""),
+    devExtended: [
+      rootVersion,
+      versionExtension,
+      "-dev",
+      process.env.GITHUB_SHA ? `-${String(process.env.GITHUB_SHA).substring(0, 7)}` : "",
+    ].join(""),
     nightly: [
       rootVersion,
+      `-${getDateString()}`,
+      process.env.GITHUB_SHA ? `-${String(process.env.GITHUB_SHA).substring(0, 7)}` : "",
+    ].join(""),
+    nightlyExtended: [
+      rootVersion,
+      versionExtension,
       `-${getDateString()}`,
       process.env.GITHUB_SHA ? `-${String(process.env.GITHUB_SHA).substring(0, 7)}` : "",
     ].join(""),
